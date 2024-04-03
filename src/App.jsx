0d0d45fc3efde61dc42fcf4638c08ea139c7a2a1
sumonelse <sumonelse01@gmail.com>
2024-04-03 19:43:52 +0530

    Uploading first React Project

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import {v4 as uuidv4} from 'uuid'
+
+import "./App.css"
+
+const App = () => {
+    const [input, setInput] = useState("")
+    const [todos, setTodos] = useState([{txt: "Create Todo", id: uuidv4()}])
+
+    const handleInput = (e) => {
+        setInput(e.target.value)
+        // console.log(input)
+    }
+
+    const addTodo = () => {
+        /** ONE WAY */
+        // const newTodos = [...todos, {txt: input, id: uuidv4()}]
+        // setTodos(newTodos)
+
+        /** ANOTHER WAY */
+        setTodos((prevTodos) => [...prevTodos, {txt: input, id: uuidv4()}])
+
+        setInput("")
+
+        // console.log(todos)
+    }
+
+    const deleteTodo = (e) => {
+        // console.log(e.target.parentNode.parentNode.querySelector(".todoTxt").innerHTML)
+        const txt = e.target.parentNode.parentNode.querySelector(".todoTxt").innerHTML
+        const newTodo = todos.filter((todo) => todo.txt != txt)
+        // console.log(newTodo)
+        setTodos(newTodo)
+        // console.log("Delete")
+    }
+
+    // console.log("RENDER TiMe: ", todos)
+
+    return (
+        <div className="container flex">
+            <div className="wrapper flex">
+                <div className="input-wrapper flex">
+                    <input type="text" onChange={(e) => handleInput(e)} value={input} placeholder="e.g. Write some Code :)"/>
+                    <button onClick={addTodo} className="btn">Add</button>
+                </div>
+                <div className="todo-wrapper flex">
+                    {todos.map((todo) => (
+                        <div key={todo.id} className="todo flex">
+                            <div className="todoTxt">{todo.txt}</div>
+                            <div className="btn-wrapper flex">
+                                <button className="editBtn btn">Edit</button>
+                                <button onClick={(e) => deleteTodo(e)} className="deleteBtn btn">Delete</button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default App
